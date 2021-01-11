d213613573735e602ef8de4fa425a1fbbbd4d72c
Mariebouquin Sriphanphet <69067303+marie1st@users.noreply.github.com>
2021-01-11 12:35:29 +0700

    Add files via upload

--- app/Models/customer.php
+++ app/Models/customer.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class customer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tax_id',
+        'email',
+        'customer_file',
+        'customer_file1',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        //
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        //
+    ];
 }
